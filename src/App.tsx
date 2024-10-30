9de5cb959f01b68dd26c9636ff28fb6819d96eda
Rodrigo Silverio <rodrigo.s.silverio@outlook.com>
2024-10-30 14:10:20 -0700

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import './global.css'
 import logo from '../public/logo-white.svg'
+
 import { Helmet, HelmetProvider } from 'react-helmet-async'
+
 
 
 export function App() {
   return (
     <HelmetProvider>
-    <Helmet titleTemplate="%s | Bolsa Click" />
-    <div className='w-full flex min-h-screen justify-center items-center flex-col antialiased'>
-      <img src={logo} alt="React logo" />
-    </div>
-  </HelmetProvider>
+      <Helmet titleTemplate="%s | Bolsa Click" />
+      <div className='w-full flex min-h-screen justify-center items-center flex-col antialiased'>
+        <img src={logo} alt="React logo" />
+      </div>
+    </HelmetProvider>
   )
 }
